1e739038d29fdc9f3b706181289ce7d753480c6b
yuler <is.yuler@gmail.com>
2026-08-03 13:30:48 +0800

    💬 [core] Rename Rails Starter branding to MonoSolo

--- core/app/mailers/application_mailer.rb
+++ core/app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: email_address_with_name(ENV.fetch("EMAIL_SENDER", "sender@example.com"), ENV.fetch("SITE_NAME", "Rails Starter"))
+  default from: email_address_with_name(ENV.fetch("EMAIL_SENDER", "sender@example.com"), ENV.fetch("SITE_NAME", "MonoSolo"))
 
   layout "mailer"
 end
