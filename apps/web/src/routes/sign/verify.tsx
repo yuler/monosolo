d7b6494cd3a85f2136a53461df4ca73bd8a77d24
yuler <6996341+yuler@users.noreply.github.com>
2026-08-07 06:40:18 +0000

    💄 [ci] Autofix code format

--- apps/web/src/routes/sign/verify.tsx
+++ apps/web/src/routes/sign/verify.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/sign/verify')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/sign/verify")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/sign/verify"!</div>
+	return <div>Hello "/sign/verify"!</div>;
 }
